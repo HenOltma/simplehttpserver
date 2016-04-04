5bf6c9568ebe27068f5fba4e14beed313955af72
NiclasvanEyk <niclas.eyk@gmail.com>
2016-04-04 14:44:10 +0200

    initial commit

---
+++ Simple HTTP Server/server.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   server.c
+ * Author: jannieyk
+ *
+ * Created on 4. April 2016, 14:37
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+

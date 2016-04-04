0a5c816303383480b9813b7ed0ed1dc948ef069a
henoltma <henoltma@si0024-19a.intra.edvsz>
2016-04-04 15:34:14 +0200

    Includes + einfacher Socket

--- Simple HTTP Server/server.c
+++ Simple HTTP Server/server.c
@@ -7,12 +7,72 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include "filereader.h"
 
+#define SRV_PORT 8998
+#define MAX_SOCK 10
+#define MAXLINE 512
+
+
+void dg_echo (int);
+void err_abort (char *str);
+void exit (int code);
+void *memset (void *s, int c, size_t n);
 /*
  * 
  */
 int main(int argc, char** argv) {
-    
+    // Deskriptoren, Adresslaenge, Prozess-ID
+    int sockfd, newsockfd, alen, pid;
+    int reuse = 1;
+    // Socket Adressen
+    struct sockaddr_in cli_addr, srv_addr;
+    // TCP-Socket erzeugen
+    if((sockfd = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
+        err_abort("Kann Stream-Socket nicht oeffnen!");
+    }
+    // Nur zum Test: Socketoption zum sofortigen Freigeben der Sockets
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <0){
+        err_abort("Kann Socketoption nicht setzen!");
+    }
+    // Binden der lokalen Adresse damit Clients uns erreichen
+    memset((void *)&srv_addr, '\0', sizeof(srv_addr));
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_addr.s_addr = htonl (INADDR_ANY);
+    srv_addr.sin_port =htons(SRV_PORT);
+    if(bind(sockfd, (struct sockaddr *)&srv_addr,
+            sizeof(srv_addr)) < 0) {
+        err_abort("Kann lokale Adresse nicht binden, laeuft fremder Server?");
+    }
     return (EXIT_SUCCESS);
 }
 
+void err_abort(char *str){
+    fprintf(stderr," TCP Echo-Server: %s\n",str);
+    fflush(stdout);
+    fflush(stderr);
+    exit(1);
+}
+
+void dg_echo (int sockfd) {
+    int alen,n;
+    char in[MAXLINE+6], out[MAXLINE+6];
+    struct sockaddr_in cli_addr;
+    
+    for(;;) {
+        alen = sizeof (cli_addr);
+        memset((void *)&in,'\0',sizeof(in));
+        //Daten vom Socket lesen
+        n = recvfrom (sockfd, in, MAXLINE, 0,
+                (struct sockaddr *)& cli_addr, &alen);
+        if(n < 0) {
+            err_abort ("Fehler beim Lesen des Sockets!");
+        }
+        sprintf (out, "Echo: %s", in);
+
+    }
+}
